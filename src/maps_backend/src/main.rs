f079be1876cf8a7bc21835e527d5d659499f7186
Barrett <onkoe@protonmail.com>
2025-10-15 11:45:27 -0500

    test(maps): add json format ck + ros 2 pub sample

--- src/maps_backend/src/main.rs
+++ src/maps_backend/src/main.rs
@@ -164,3 +164,192 @@ async fn start_websocket_server(mut rx: broadcast::Receiver<String>) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use core::{
+        assert_eq,
+        f64::consts::PI,
+        sync::atomic::{AtomicBool, Ordering},
+        time::Duration,
+    };
+    use std::{sync::Arc, time::SystemTime};
+
+    use safe_drive::{
+        context::Context,
+        msg::{
+            builtin_interfaces::UnsafeTime,
+            common_interfaces::{
+                sensor_msgs::msg::{NavSatFix, NavSatStatus},
+                std_msgs::msg::Header,
+            },
+            RosString,
+        },
+        node::Node,
+        qos::Profile,
+        topic::subscriber::Subscriber,
+    };
+    use tokio::{sync::broadcast, time::Timeout};
+
+    /// Checks that we only create JSON floats -- not numbers that can be
+    /// parsed as integers!
+    #[test]
+    fn creates_valid_json_floats() {
+        // init logging
+        _ = tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::TRACE)
+            .try_init();
+
+        // use these pairs
+        #[expect(
+            clippy::type_complexity,
+            reason = "types are ugly, but who cares? it's a test man"
+        )]
+        const COORD_RESULT_PAIRS: &[((f64, f64), (&str, &str))] = &[
+            ((1.0, 2.0), ("1.0", "2.0")),
+            ((PI, -43.67219), ("3.141592653589793", "-43.67219")),
+            ((0_f64, 0_f64), ("0.0", "0.0")),
+        ];
+
+        // check that each pair matches expectations
+        for ((lat, lon), (lat_str, lon_str)) in COORD_RESULT_PAIRS {
+            let as_json: String = super::create_json_coordinate_pair(*lat, *lon);
+            let split = as_json.split(", ").collect::<Vec<_>>();
+            let [lat_json, lon_json] = split.as_slice() else {
+                panic!();
+            };
+
+            assert_eq!(lat_json, &format!(r#"{{"lat": {lat_str}"#).as_str());
+            assert_eq!(lon_json, &format!(r#""lon": {lon_str}}}"#).as_str());
+        }
+    }
+
+    /// Ensures that we receive ROS 2 messages correctly.
+    ///
+    /// This test:
+    ///
+    /// - Creates the `maps_node`
+    /// - Creates a fake "sensors" publisher node
+    /// - Fails if the `maps_node` fails to get all messages within time limit
+    ///
+    /// Note that this is a `tokio` test, so it's async. That means we have
+    /// full access to its async runtime, and tasks can run in the background!
+    #[tokio::test]
+    async fn recv_ros2_messages() {
+        // init logging
+        _ = tracing_subscriber::fmt()
+            .with_max_level(tracing::Level::TRACE)
+            .try_init();
+
+        // create shared bool (so we can wait for both threads to be ready)
+        let is_ready: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+
+        // create channels to get json output
+        let (tx, mut rx): (broadcast::Sender<String>, broadcast::Receiver<String>) =
+            broadcast::channel::<String>(16);
+
+        let ctx: Arc<Context> = Context::new().unwrap();
+
+        // create a lil sender node to test it out
+        const MAX_MESSAGES: i32 = 10;
+        let ctx_2 = Arc::clone(&ctx);
+        let is_ready_2: Arc<AtomicBool> = Arc::clone(&is_ready); // ugly b/c of `move` rules
+        std::thread::spawn(move || {
+            let sender_node: Arc<Node> = ctx_2
+                .create_node(
+                    "maps_backend_sender_test_node",
+                    Some("/"),
+                    Default::default(),
+                )
+                .unwrap();
+
+            let gps_publisher = sender_node
+                .create_publisher::<NavSatFix>(
+                    "/sensors/gps/rust_test",
+                    Some(Profile::sensor_data()),
+                )
+                .unwrap();
+
+            // wait until the other node is up
+            while !is_ready_2.load(Ordering::SeqCst) {
+                std::thread::sleep(Duration::from_millis(5));
+            }
+            std::thread::sleep(Duration::from_millis(500));
+
+            for i in 0..MAX_MESSAGES {
+                let timestamp: Duration = SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap();
+
+                if timestamp.as_secs() > (i32::MAX as u64) {
+                    panic!("year 2038 bug detected (or system time is very wrong)");
+                }
+
+                let (lat, lon): (f64, f64) = (i as f64 + 0.5, i as f64 + 2.3);
+
+                let n: NavSatFix = NavSatFix {
+                    header: Header {
+                        stamp: UnsafeTime {
+                            sec: timestamp.as_secs() as i32,
+                            nanosec: timestamp.subsec_nanos(),
+                        },
+                        frame_id: RosString::new("im a gps frame").unwrap(),
+                    },
+                    status: NavSatStatus::new().unwrap(),
+                    latitude: lat,
+                    longitude: lon,
+                    altitude: i as f64 + 624.0153,
+                    position_covariance: [0_f64; 9],
+                    position_covariance_type: 0,
+                };
+
+                tracing::info!("sender node: loop {i}: sending coord: ({lat:?}, {lon:?})");
+                gps_publisher.send(&n).expect("send coord");
+                std::thread::sleep(Duration::from_millis(20));
+            }
+        });
+
+        // then, create the maps node
+        let node: Arc<Node> = ctx
+            .create_node("maps_backend_test_node", Some("/"), Default::default())
+            .unwrap();
+        let gps_subscriber: Subscriber<NavSatFix> = node
+            .create_subscriber::<NavSatFix>("/sensors/gps/rust_test", Some(Profile::sensor_data()))
+            .unwrap();
+
+        // spawn the maps node in the background.
+        //
+        // this is the node we're testing.
+        tokio::task::spawn(super::handle_ros2_messages(gps_subscriber, tx));
+        is_ready.store(true, Ordering::SeqCst);
+
+        // helper func for below...
+        let helper_require_ten_msgs_within_time: Timeout<_> = tokio::time::timeout(
+            Duration::from_millis(1000),
+            tokio::task::spawn(async move {
+                let mut counter = 0;
+                loop {
+                    let _ = rx.recv().await.expect("should get json successfully");
+                    counter += 1;
+
+                    // stop looking if we're @ max messages
+                    if counter >= MAX_MESSAGES {
+                        return;
+                    }
+                }
+            }),
+        );
+
+        // ensure we get ten messages within required time.
+        //
+        // if not, panic (which fails the test)
+        match helper_require_ten_msgs_within_time.await {
+            Ok(_) => {
+                tracing::info!("Test completed successfully!");
+            }
+            Err(e) => {
+                panic!("Reached timeout, or got err! err: {e}!");
+            }
+        };
+    }
+}
